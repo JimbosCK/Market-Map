0976f6242d35278c86702602261f9e8cd79f8a38
Dimitris C. K <dimchatzkar@gmail.com>
2025-08-08 17:17:34 +0300

    Get verified and Delete by id routes/controllers added

--- server/src/controllers/market.controller.js
+++ server/src/controllers/market.controller.js
@@ -1,5 +1,7 @@
 const Market = require('../models/market.model');
 
+// TODO: Write authorization middleware to protect some routes
+
 const createMarket = async (req, res) => {
   try {
     const newMarket = new Market(req.body);
@@ -19,7 +21,32 @@ const getMarkets = async (req, res) => {
   }
 };
 
+const getVerifiedMarkets = async (req, res) => {
+  try {
+    const verifiedMarkets = await Market.find({ status: 'verified' });
+    res.status(200).json(verifiedMarkets);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const deleteMarket = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedMarket = await Market.findByIdAndDelete(id);
+    if (!deletedMarket) {
+      return res.status(404).json({ message: 'Market not found' });
+    }
+    res.status(200).json({ message: 'Market deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   createMarket,
   getMarkets,
+  getVerifiedMarkets,
+  deleteMarket,
+
 };
--- server/src/routes/markets.routes.js
+++ server/src/routes/markets.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createMarket, getMarkets } = require('../controllers/market.controller');
+const { createMarket, getMarkets, getVerifiedMarkets, deleteMarket } = require('../controllers/market.controller');
 
 router.post('/', createMarket);
 
 router.get('/', getMarkets);
+
+router.get('/verified', getVerifiedMarkets);
+router.delete('/:id', deleteMarket);
 
 module.exports = router;
