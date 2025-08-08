5d64e005ca65c370f4c6238b17552a060a14ff4c
Dimitris C. K <dimchatzkar@gmail.com>
2025-08-08 17:17:11 +0300

    Server refactoring

--- server/src/server.js
+++ server/src/server.js
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected successfully!');
   })
@@ -30,6 +27,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/markets', marketsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
